9faa2a324ebce6597f7a6767da86c5cc1b1ff020
Iria <iria.pardo.neira@udc.es>
2024-05-01 13:34:49 +0200

    Final deep learning
    Co-authored-by: eduardofernandezfraga <eduardofernandezfraga@users.noreply.github.com>
    Co-authored-by: xianpriego <xianpriego@users.noreply.github.com>
    Co-authored-by: ruben-fernandez-farelo <ruben-fernandez-farelo@users.noreply.github.com>
    Co-authored-by: Ana Bañobre Martínez <anabanobremtnz@users.noreply.github.com>

--- src/deepLearning.jl
+++ src/deepLearning.jl
@@ -4,9 +4,8 @@ using Flux: onehotbatch, onecold, adjust!
 using JLD2, FileIO
 using Statistics: mean
 
-Random.seed!(123)
-
 include("adhoc.jl")
+include("arquitectures.jl")
 
 function separador_dir()
     if Sys.iswindows()
@@ -33,8 +32,21 @@ instances = length(no_melanoma_color) + length(melanoma_color) + length(atypical
 index = shuffle(1:instances)
 inputs_color, targets = crear_inputs_targets_multiclase(no_melanoma_color, melanoma_color, atypical_color, index)
 
+ruta_destino = "resized_images"*sep
+ruta_destino2 = "images"*sep
 #Resize imagenes al mismo tamaño
-inputs_color = [imresize(img, (570, 760)) for img in inputs_color]
+#=
+for i in 1:length(inputs_color)
+    ruta_completa1 = joinpath(ruta_destino2, string(i)*".png")
+    save(ruta_completa1, inputs_color[i])
+    inputs_color[i] = imresize(inputs_color[i], (30, 30))
+    ruta_completa = joinpath(ruta_destino, string(i)*".png")
+    save(ruta_completa, inputs_color[i])
+end
+=#
+
+inputs_color = [imresize(img, (30, 30)) for img in inputs_color]
+
 
 #Hold out para dividir en train y test porque no se realiza validación cruzada
 index_train, index_test = holdOut(length(inputs_color), 0.2)
@@ -53,13 +65,12 @@ test_imgs = imageToColorArray.(inputs_color[index_test])
 train_labels = targets[index_train]
 test_labels = targets[index_test]
 
-println(test_labels)
 
 function convertirArrayImagenesHWCN(imagenes)
     numPatrones = length(imagenes);
-    nuevoArray = Array{Float32,4}(undef, 570, 760, 3, numPatrones); # Importante que sea un array de Float32
+    nuevoArray = Array{Float32,4}(undef, 30, 30, 3, numPatrones); # Importante que sea un array de Float32
     for i in 1:numPatrones
-        @assert (size(imagenes[i])==(570,760,3)) "Las imagenes no tienen tamaño 570x760";
+        @assert (size(imagenes[i])==(30,30,3)) "Las imagenes no tienen tamaño 50x50";
         nuevoArray[:,:,:,i] .= imagenes[i];
     end;
     return nuevoArray;
@@ -71,138 +82,79 @@ test_imgs = convertirArrayImagenesHWCN(test_imgs);
 labels = unique(targets)
 train_set = (train_imgs, onehotbatch(train_labels, labels))
 test_set = (test_imgs, onehotbatch(test_labels, labels))
-println(test_set[2]')
-
 
 println("Tamaño de la matriz de entrenamiento: ", size(train_imgs))
 println("Tamaño de la matriz de test:          ", size(test_imgs))
 
 println("Valores minimo y maximo de las entradas: (", minimum(train_imgs), ", ", maximum(train_imgs), ")");
 
-funcionTransferenciaCapasConvolucionales = relu;
 
-# Definimos la red con la funcion Chain, que concatena distintas capas
-ann1 = Chain(
-
-    Conv((3, 3), 3=>16, pad=(1,1), funcionTransferenciaCapasConvolucionales),
-
-    #570, 760 -> 285, 380
-    MaxPool((2,2)),
-
-    Conv((3, 3), 16=>32, pad=(1,1), funcionTransferenciaCapasConvolucionales),
-
-    #285, 380 -> 142, 190
-    MaxPool((2,2)),
-
-    Conv((3, 3), 32=>32, pad=(1,1), funcionTransferenciaCapasConvolucionales),
-
-    #142, 190 -> 71, 95
-    MaxPool((2,2)),
+function train(ann)
     
-    x -> reshape(x, :, size(x, 4)),
+    # Definimos la funcion de loss de forma similar a las prácticas de la asignatura
+    L1 = 0.001
+    L2 = 0.001
 
-    #71*95*32
-    Dense(215840, 3),
+    absnorm(x) = sum(abs , x)
+    sqrnorm(x) = sum(abs2, x)
+    loss(model,x,y) = ((size(y,1) == 1) ? Losses.binarycrossentropy(model(x),y) : Losses.crossentropy(model(x),y)) + L1*sum(absnorm, Flux.params(model)) + L2*sum(sqrnorm, Flux.params(model));
+    eta = 0.01;
+    opt_state = Flux.setup(Adam(eta), ann);
 
-    softmax
-)
+    println("Comenzando entrenamiento con arquitectura ", ann)
+    mejorPrecision = -Inf;
+    criterioFin = false;
+    numCiclo = 0;
+    numCicloUltimaMejora = 0;
+    mejorModelo = nothing;
 
-ann = Chain(
+    while !criterioFin
 
-    Conv((61, 81), 3=>16, pad=(1,1), funcionTransferenciaCapasConvolucionales),
-
-    #570, 760 -> 285, 380
-    MaxPool((2,2)),
-
-    Conv((3, 3), 16=>32, pad=(1,1), funcionTransferenciaCapasConvolucionales),
-
-    #285, 380 -> 142, 190
-    MaxPool((2,2)),
-
-    Conv((3, 3), 32=>32, pad=(1,1), funcionTransferenciaCapasConvolucionales),
-
-    #142, 190 -> 71, 95
-    MaxPool((2,2)),
+        # Hay que declarar las variables globales que van a ser modificadas en el interior del bucle
+        #global numCicloUltimaMejora, numCiclo, mejorPrecision, mejorModelo, criterioFin;
     
-    x -> reshape(x, :, size(x, 4)),
-
-    #71*95*32
-    Dense(215840, 3),
-
-    softmax
-)
-
-
-
-# Definimos la funcion de loss de forma similar a las prácticas de la asignatura
-loss(ann1, x, y) = (size(y,1) == 1) ? Losses.binarycrossentropy(ann1(x),y) : Losses.crossentropy(ann1(x),y);
-eta = 0.01;
-opt_state = Flux.setup(Adam(eta), ann1);
-
-println("salidas deseadas:")
-println(test_set[2])
-println("size salidas ann:")
-println(size((ann1(test_set[1]))))
-println("Salidas ann")
-println((ann1(test_set[1])))
-println(onecold(ann1(test_set[1])))
-
-println("Comenzando entrenamiento...")
-mejorPrecision = -Inf;
-criterioFin = false;
-numCiclo = 0;
-numCicloUltimaMejora = 0;
-mejorModelo = nothing;
-
-
-while !criterioFin
-
-    # Hay que declarar las variables globales que van a ser modificadas en el interior del bucle
-    global numCicloUltimaMejora, numCiclo, mejorPrecision, mejorModelo, criterioFin;
-
-    # Se entrena un ciclo
-    Flux.train!(loss, ann1, [train_set], opt_state);
-
-    numCiclo += 1;
-
-    # Se calcula la precision en el conjunto de entrenamiento:
-    #precisionEntrenamiento = mean(onecold(ann1(train_set[1])) .== onecold(train_set[2]));
-    println("hola")
-    precisionEntrenamiento = accuracy(ann1(train_set[1])', train_set[2]')
-    println("Ciclo ", numCiclo, ": Precision en el conjunto de entrenamiento: ", 100*precisionEntrenamiento, " %");
-
-    # Si se mejora la precision en el conjunto de entrenamiento, se calcula la de test y se guarda el modelo
-    if (precisionEntrenamiento > mejorPrecision)
-        mejorPrecision = precisionEntrenamiento;
-        println(onecold(ann1(test_set[1])))
-        println(test_set[2])
-        println(onecold(test_set[2]))
-        precisionTest = mean(onecold(ann1(test_set[1])) .== onecold(test_set[2]));
-        println("   Mejora en el conjunto de entrenamiento -> Precision en el conjunto de test: ", 100*precisionTest, " %");
-        mejorModelo = deepcopy(ann1);
-        numCicloUltimaMejora = numCiclo;
+        # Se entrena un ciclo
+        Flux.train!(loss, ann, [train_set], opt_state);
+    
+        numCiclo += 1;
+    
+        # Se calcula la precision en el conjunto de entrenamiento:
+    
+        precisionEntrenamiento = accuracy(ann(train_set[1])', train_set[2]')
+        #println("Ciclo ", numCiclo, ": Precision en el conjunto de entrenamiento: ", 100*precisionEntrenamiento, " %");
+    
+        # Si se mejora la precision en el conjunto de entrenamiento, se calcula la de test y se guarda el modelo
+        if (precisionEntrenamiento > mejorPrecision)
+            mejorPrecision = precisionEntrenamiento;
+            precisionTest = mean(onecold(ann(test_set[1])) .== onecold(test_set[2]));
+            #println("   Mejora en el conjunto de entrenamiento -> Precision en el conjunto de test: ", 100*precisionTest, " %");
+            mejorModelo = deepcopy(ann);
+            numCicloUltimaMejora = numCiclo;
+        end
+        
+        # Criterios de parada:
+    
+        # Si la precision en entrenamiento es lo suficientemente buena, se para el entrenamiento
+        if (precisionEntrenamiento >= 0.999)
+            println("   Se para el entenamiento por haber llegado a una precision de 99.9%")
+            criterioFin = true;
+            return precisionTest
+        end
+    
+        # Si no se mejora la precision en el conjunto de entrenamiento durante 10 ciclos, se para el entrenamiento
+        if (numCiclo - numCicloUltimaMejora >= 15)
+            precisionTest = mean(onecold(ann(test_set[1])) .== onecold(test_set[2]));
+            println("   Se para el entrenamiento por no haber mejorado la precision en el conjunto de entrenamiento durante 15 ciclos con una precisión en test de ", 100*precisionTest, " %")
+            criterioFin = true;
+            return precisionTest
+        end
+        
     end
-
-    # Si no se ha mejorado en 5 ciclos, se baja la tasa de aprendizaje
-    if (numCiclo - numCicloUltimaMejora >= 5) && (eta > 1e-6)
-        global eta
-        eta /= 10.0
-        println("   No se ha mejorado la precision en el conjunto de entrenamiento en 5 ciclos, se baja la tasa de aprendizaje a ", eta);
-        adjust!(opt_state, eta)
-        numCicloUltimaMejora = numCiclo;
-    end
-
-    # Criterios de parada:
-
-    # Si la precision en entrenamiento es lo suficientemente buena, se para el entrenamiento
-    if (precisionEntrenamiento >= 0.999)
-        println("   Se para el entenamiento por haber llegado a una precision de 99.9%")
-        criterioFin = true;
-    end
-
-    # Si no se mejora la precision en el conjunto de entrenamiento durante 10 ciclos, se para el entrenamiento
-    if (numCiclo - numCicloUltimaMejora >= 10)
-        println("   Se para el entrenamiento por no haber mejorado la precision en el conjunto de entrenamiento durante 10 ciclos")
-        criterioFin = true;
-    end
+    
 end
+
+results = [train(ann) for ann in arquitecturas]
+
+println(results)
+
+
